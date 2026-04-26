0ac6bb0c71094bd1cc6b9cf0fe77150585bf5b54
maaru <maratarzymatov288@gmail.com>
2026-04-26 19:44:14 +0600

    fixed something

--- lua/plugins/dev/tree_sitter/syntax_highlight.lua
+++ lua/plugins/dev/tree_sitter/syntax_highlight.lua
@@ -1,64 +1,79 @@
+local parsers = {
+	"c",
+	"cpp",
+	"regex",
+	"lua",
+	"vim",
+	"vimdoc",
+	"query",
+	"markdown",
+	"markdown_inline",
+	"python",
+	"javascript",
+	"typescript",
+	"html",
+	"htmldjango",
+	"css",
+	"bash",
+	"json",
+	"sql",
+	"dockerfile",
+	"yaml",
+	"toml",
+	"rust",
+	"go",
+	"todotxt",
+	"hyprlang",
+	"vue",
+	"tsx",
+	"svelte",
+	"astro",
+}
+
 return {
 	"nvim-treesitter/nvim-treesitter",
+
+	branch = "main",
+	version = false,
+	lazy = false,
+
 	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
-		ensure_installed = {
-			"c",
-			"cpp",
-			"regex",
-			"lua",
-			"vim",
-			"vimdoc",
-			"query",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"javascript",
-			"typescript",
-			"html",
-			"htmldjango",
-			"css",
-			"bash",
-			"json",
-			"sql",
-			"dockerfile",
-			"yaml",
-			"toml",
-			"rust",
-			"go",
-			"todotxt",
-			"hyprlang",
-			"vue",
-			"tsx",
-			"svelte",
-			"astro",
-		},
 
-		sync_install = false,
-		auto_install = true,
-
-		highlight = {
-			enable = true,
-			disable = function(_, buf)
-				local max_filesize = 100 * 1024
-				local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-				return ok and stats and stats.size > max_filesize
-			end,
-			additional_vim_regex_highlighting = false,
-		},
-
-		indent = {
-			enable = true,
-		},
-	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-
+	config = function()
 		vim.filetype.add({
 			pattern = {
 				[".*/hypr/.*%.conf"] = "hyprlang",
 			},
+		})
+
+		local ts = require("nvim-treesitter")
+
+		ts.setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+
+		ts.install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("MoonlightTreesitter", { clear = true }),
+			callback = function(args)
+				local max_filesize = 100 * 1024
+				local filename = vim.api.nvim_buf_get_name(args.buf)
+
+				if filename ~= "" then
+					local ok, stats = pcall(vim.uv.fs_stat, filename)
+
+					if ok and stats and stats.size > max_filesize then
+						return
+					end
+				end
+
+				pcall(vim.treesitter.start, args.buf)
+
+				pcall(function()
+					vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end)
+			end,
 		})
 	end,
 }
--- lua/plugins/tools/waka_time.lua
+++ lua/plugins/tools/waka_time.lua
@@ -1,9 +1,28 @@
 return {
 	"wakatime/vim-wakatime",
 	lazy = false,
-	opts = {
-		debug = false,
-		redraw_setting = "disabled",
-		heartbeat_frequency = 5,
-	},
+
+	init = function()
+		vim.g.loaded_wakatime = 1
+
+		local notify = vim.notify
+
+		---@diagnostic disable-next-line: duplicate-set-field
+		vim.notify = function(msg, level, opts)
+			if type(msg) == "string" and msg:find("%[WakaTime%]") then
+				return
+			end
+
+			return notify(msg, level, opts)
+		end
+	end,
+
+	config = function()
+		require("wakatime").setup({
+			debug = false,
+			redraw_setting = "disabled",
+			heartbeat_frequency = 5,
+			status_bar_enabled = false,
+		})
+	end,
 }
