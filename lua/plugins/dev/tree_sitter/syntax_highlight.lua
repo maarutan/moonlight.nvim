cac769cb005d9a0ae684bd824aa749742bcccdff
maaru <maratarzymatov288@gmail.com>
2026-05-17 18:00:56 +0600

    fixed TS hl

--- lua/plugins/dev/tree_sitter/syntax_highlight.lua
+++ lua/plugins/dev/tree_sitter/syntax_highlight.lua
@@ -4,64 +4,30 @@ return {
 		lazy = false,
 		build = ":TSUpdate",
 
-		opts = {
-			ensure_installed = {
-				"c",
-				"cpp",
-				"regex",
-				"lua",
-				"vim",
-				"vimdoc",
-				"query",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"javascript",
-				"typescript",
-				"html",
-				"htmldjango",
-				"css",
-				"bash",
-				"json",
-				"sql",
-				"dockerfile",
-				"yaml",
-				"toml",
-				"rust",
-				"go",
-				"todotxt",
-				"hyprlang",
-				"vue",
-				"tsx",
-				"svelte",
-				"astro",
-			},
-
-			sync_install = false,
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-				disable = function(_, buf)
-					local max_filesize = 100 * 1024
-					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-					return ok and stats and stats.size > max_filesize
-				end,
-				additional_vim_regex_highlighting = false,
-			},
-
-			indent = {
-				enable = true,
-			},
-		},
-
-		config = function(_, opts)
-			require("nvim-treesitter").setup(opts)
+		config = function()
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
+			})
 
 			vim.filetype.add({
 				pattern = {
 					[".*/hypr/.*%.conf"] = "hyprlang",
 				},
+			})
+
+			local aug = vim.api.nvim_create_augroup("MyTreeSitter", { clear = true })
+
+			vim.api.nvim_create_autocmd("FileType", {
+				group = aug,
+				callback = function(args)
+					local buf = args.buf
+
+					pcall(vim.treesitter.start, buf)
+
+					if vim.bo[buf].buftype == "" then
+						vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end,
 			})
 		end,
 	},
